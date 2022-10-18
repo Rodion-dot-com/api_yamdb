5d3ca860a44467097a1116ec6325994bbdf018c7
Павел Новиков <pasha_n2006@mail.ru>
2022-10-18 22:32:59 +0300

    1. Добавил в api\views вьюсеты ReviewViewSet и CommentViewSet 2. Добавил в api\serializers сериализаторы для указанных вьюсетов + сделал валидацию на score (тип и диапазон от 1 до 10)

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+
+from reviews.models import Review, Comment
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Review
+
+    def validate_score(self, data):
+        score = data.get('score')
+        if type(score) is not int or score not in range(1, 11):
+            raise serializers.ValidationError(
+                ('Оценка произведения должна быть целой цифрой в диапазоне '
+                 'от 1 до 10')
+            )
+        return data
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        field = '__all__'
+        model = Comment
