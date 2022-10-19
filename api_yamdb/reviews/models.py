c61d59d0f36579e35d611b4beaa4e5620486a58e
Mark Zotov <zotov001@yandex.ru>
2022-10-19 18:46:24 +0300

    AbstractUser in reviews/model

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,11 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.models import User
- 
- 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=30)
-    bio = models.TextField()
- 
-    def __str__(self):
-        return self.user.username
+
+
+class User(AbstractUser):
+    bio = models.TextField(
+        'Биография',
+        blank=True,
+    )
+    role = models.CharField(
+        max_length=30,
+    )
