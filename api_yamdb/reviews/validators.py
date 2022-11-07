66855e696f20031676a27bfe1f4de7199d32ef5b
AnastasiaMaksimova <m_stue@mail.ru>
2022-11-07 19:42:58 +0300

    updated validate_year

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -4,5 +4,5 @@ from django.core.exceptions import ValidationError
 
 
 def validate_year(value):
-    if value < MINYEAR or value > datetime.now().year:
+    if not MINYEAR <= value <= datetime.now().year:
         raise ValidationError('Год указан неправильно')
